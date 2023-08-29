300aea24dc691b69247a058e0a96c3d4b938e1d0
mamatha1409 <124038311+mamatha1409@users.noreply.github.com>
2023-08-29 09:43:07 +0530

    Add files via upload

---
+++ Dealer.java
@@ -0,0 +1,173 @@
+
+
+/**
+ * Class Central_System
+ */
+public class Central_System {
+
+  //
+  // Fields
+  //
+
+  private void store;
+  private void Update;
+  
+  //
+  // Constructors
+  //
+  public Central_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store
+   * @param newVar the new value of store
+   */
+  private void setStore (void newVar) {
+    store = newVar;
+  }
+
+  /**
+   * Get the value of store
+   * @return the value of store
+   */
+  private void getStore () {
+    return store;
+  }
+
+  /**
+   * Set the value of Update
+   * @param newVar the new value of Update
+   */
+  private void setUpdate (void newVar) {
+    Update = newVar;
+  }
+
+  /**
+   * Get the value of Update
+   * @return the value of Update
+   */
+  private void getUpdate () {
+    return Update;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void Storing()
+  {
+  }
+
+
+  /**
+   */
+  public void Updating()
+  {
+  }
+
+
+  /**
+   */
+  public void Processing()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class Dealer
+ */
+public class Dealer {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void availability;
+  
+  //
+  // Constructors
+  //
+  public Dealer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of availability
+   * @param newVar the new value of availability
+   */
+  private void setAvailability (void newVar) {
+    availability = newVar;
+  }
+
+  /**
+   * Get the value of availability
+   * @return the value of availability
+   */
+  private void getAvailability () {
+    return availability;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery()
+  {
+  }
+
+
+  /**
+   */
+  public void dealer()
+  {
+  }
+
+
+}
